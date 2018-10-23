db45836751a4f7cc70f4f5f0e13b7a9d52dfc677
Lishangwen <lswscu@foxmail.com>
2018-10-23 20:15:17 +0800

    Add Two Numbers

--- LeetCodeImplemention/LeetCodeLibrary.cpp
+++ LeetCodeImplemention/LeetCodeLibrary.cpp
@@ -3,6 +3,7 @@
 
 
 
+
 LeetCodeLibrary::LeetCodeLibrary()
 {
 }
@@ -24,4 +25,18 @@ vector<int> LeetCodeLibrary::twoSum(vector<int>& nums, int target)
 		hashmap[nums[i]] = i;
 	}
 	return { 0,0 };
+}
+
+ListNode* LeetCodeLibrary::addTwoNumbers(ListNode* l1, ListNode* l2)
+{
+	ListNode* result = new ListNode(0), *ptr = result;
+	while (l1 != NULL || l2 != NULL){
+		ptr->val = ptr->val+((l1 == NULL) ? 0 : l1->val)+ ((l2 == NULL) ? 0 : l2->val);
+		if (l1 != NULL) l1 = l1->next;
+		if (l2 != NULL) l2 = l2->next;
+		if(l1 != NULL || l2 != NULL|| ptr->val>=10) ptr->next = new ListNode(ptr->val / 10);
+		if (ptr->val >=10) ptr->val %= 10;
+		ptr = ptr->next;
+	}
+	return result;
 }
--- LeetCodeImplemention/LeetCodeLibrary.h
+++ LeetCodeImplemention/LeetCodeLibrary.h
@@ -1,15 +1,25 @@
 #pragma once
 #include "vector"
 #include "unordered_map"
+#include "math.h"
 using std::vector;
 using std::unordered_map;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
 class LeetCodeLibrary
 {
 public:
 	LeetCodeLibrary();
 	~LeetCodeLibrary();
 
-	// 1¡¢TwoSum
+	// 1. TwoSum
 	vector<int> twoSum(vector<int>& nums, int target);
+
+	// 2. Add Two Numbers
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
 };
 
