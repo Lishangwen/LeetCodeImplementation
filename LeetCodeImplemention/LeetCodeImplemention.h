c2a6187237502c4135e828e556c5e17a9f8bed4f
Lishangwen <lswscu@foxmail.com>
2018-10-23 20:32:22 +0800

    增加LeetcodeImplemention.h用于定义测试函数

---
+++ LeetCodeImplemention/LeetCodeImplemention.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <Vector>
+#include "LeetCodeLibrary.h"
+void TwoSumTest(LeetCodeLibrary leetCodeLib);
+void AddTwoNumbersTest(LeetCodeLibrary leetCodeLib);
